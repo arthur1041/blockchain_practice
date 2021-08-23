3d27f99f497504b3517c8a155241907196d0d7a7
Arthur Cruz <arthur.cruz@esig.com.br>
2021-08-23 01:08:28 -0300

    few changes

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -5,8 +5,15 @@ import "./Token.sol";
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
+    uint public rate = 100;
 
     constructor(Token _token) public {
         token = _token;
     }
+
+    function buyToken() public payable {
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
+    
 }
